e157dc4d6a96566b7ae18abd0fc8818b1d6152b7
Daniel Knauf <daniel.knaut@posteo.de>
2019-08-05 21:34:24 +0200

    implement_repeat_function
    - build MutableLiveData with initial value

--- app/src/main/java/knaufdan/android/simpletimerapp/ui/fragments/InputFragmentViewModel.kt
+++ app/src/main/java/knaufdan/android/simpletimerapp/ui/fragments/InputFragmentViewModel.kt
@@ -2,40 +2,36 @@ package knaufdan.android.simpletimerapp.ui.fragments
 
 import android.view.View
 import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import knaufdan.android.simpletimerapp.arch.BaseViewModel
+import knaufdan.android.simpletimerapp.databinding.ExtMutableLiveData
 import knaufdan.android.simpletimerapp.ui.navigation.Navigator
-import knaufdan.android.simpletimerapp.util.Constants.MINUTE
 import knaufdan.android.simpletimerapp.util.Constants.KEY_TIMER_STATE
+import knaufdan.android.simpletimerapp.util.Constants.MINUTE
 import knaufdan.android.simpletimerapp.util.SharedPrefService
 import knaufdan.android.simpletimerapp.util.UnBoxUtil.safeUnBox
 import knaufdan.android.simpletimerapp.util.service.TimerState
 import javax.inject.Inject
 
 class InputFragmentViewModel @Inject constructor(
-        private val navigator: Navigator,
-        sharedPrefService: SharedPrefService
+    private val navigator: Navigator,
+    sharedPrefService: SharedPrefService
 ) : BaseViewModel() {
 
-    val timePerCycle = MutableLiveData<Int?>()
+    val timePerCycle = ExtMutableLiveData<Int?>(1)
 
     val isEnabled = MediatorLiveData<Boolean>()
 
-    val isOnRepeat = MutableLiveData<Boolean>()
+    val isOnRepeat = ExtMutableLiveData(false)
 
-    val continueButtonClick: View.OnClickListener = View.OnClickListener {
+    val continueButtonClick = View.OnClickListener {
         timePerCycle.value?.let { input -> navigator.navigateToTimer(input.times(MINUTE), safeUnBox(isOnRepeat.value)) }
     }
 
     init {
         sharedPrefService.saveTo(KEY_TIMER_STATE, TimerState.RESET_STATE)
 
-        timePerCycle.value = 1
-
-        isOnRepeat.value = false
-
-        isEnabled.addSource(timePerCycle) {
-            isEnabled.postValue(it != null && it > 0)
+        isEnabled.addSource(timePerCycle) { time ->
+            isEnabled.postValue(time != null && time > 0)
         }
     }
 }
