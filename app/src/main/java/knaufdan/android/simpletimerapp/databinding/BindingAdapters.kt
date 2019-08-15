cff3febb3f3179a32e8e90925976c3f74b58d4d1
Daniel Knauf <daniel.knaut@posteo.de>
2019-08-15 07:00:10 +0200

    Refactoring the app
    - make binding adapter more kotlin

--- app/src/main/java/knaufdan/android/simpletimerapp/databinding/BindingAdapters.kt
+++ app/src/main/java/knaufdan/android/simpletimerapp/databinding/BindingAdapters.kt
@@ -1,13 +1,11 @@
 package knaufdan.android.simpletimerapp.databinding
 
-import android.annotation.SuppressLint
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import knaufdan.android.simpletimerapp.util.Constants.MINUTE
 import knaufdan.android.simpletimerapp.util.Constants.SECOND
 
-@SuppressLint("SetTextI18n")
-@BindingAdapter("progressText")
+@BindingAdapter(value = ["progressText"])
 fun TextView.setProgressText(progress: Int?) {
     text = progress?.run {
         val minutes = (this / MINUTE).addZero()
