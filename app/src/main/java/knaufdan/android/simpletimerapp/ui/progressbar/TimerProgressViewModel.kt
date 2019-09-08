593f6c58b725f6c8baa08bb4006dd4c88aab0a69
Daniel Knauf <daniel.knaut@posteo.de>
2019-09-08 20:28:41 +0200

    bug-fix
    - fix calculation

--- app/src/main/java/knaufdan/android/simpletimerapp/ui/progressbar/TimerProgressViewModel.kt
+++ app/src/main/java/knaufdan/android/simpletimerapp/ui/progressbar/TimerProgressViewModel.kt
@@ -6,7 +6,8 @@ class TimerProgressViewModel : ProgressBarViewModel {
     override val maximum = ExtMutableLiveData(0)
     override val progress = ExtMutableLiveData(0)
 
-    override fun increaseProgress(increment: Int) = progress.postValue(progress.value?.plus(increment))
+    override fun increaseProgress(increment: Int) =
+        progress.postValue(progress.value?.plus(increment))
 
-    override fun calculateRemainingProgress() = progress.value?.apply { maximum.value?.minus(this) }?.toLong() ?: 0
+    override fun calculateRemainingProgress() = progress.value?.run { maximum.value?.minus(this) }?.toLong() ?: 0
 }
