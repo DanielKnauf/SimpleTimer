de9f85ebe9182ab07825cd9f86e5be4e9dd26324
Daniel Knauf <daniel.knaut@posteo.de>
2019-10-07 06:46:52 +0200

    Switch to count down
    - get rid of reset button
    - switch from counting up to counting down by changing logic for display

--- app/src/main/java/knaufdan/android/simpletimerapp/ui/progressbar/TimerProgressViewModel.kt
+++ app/src/main/java/knaufdan/android/simpletimerapp/ui/progressbar/TimerProgressViewModel.kt
@@ -1,13 +1,22 @@
 package knaufdan.android.simpletimerapp.ui.progressbar
 
+import androidx.lifecycle.MediatorLiveData
 import knaufdan.android.simpletimerapp.databinding.ExtMutableLiveData
 
 class TimerProgressViewModel : ProgressBarViewModel {
     override val maximum = ExtMutableLiveData(0)
     override val progress = ExtMutableLiveData(0)
+    override val inverseProgress = MediatorLiveData<Int>()
+
+    init {
+        inverseProgress.addSource(progress) { progress ->
+            inverseProgress.postValue(maximum.value?.minus(progress) ?: 0)
+        }
+    }
 
     override fun increaseProgress(increment: Int) =
         progress.postValue(progress.value?.plus(increment))
 
-    override fun calculateRemainingProgress() = progress.value?.run { maximum.value?.minus(this) }?.toLong() ?: 0
+    override fun calculateRemainingProgress() =
+        progress.value?.run { maximum.value?.minus(this) }?.toLong() ?: 0
 }
