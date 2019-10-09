ac9b6d45026e5f7640a5266fff2193f9a8f30338
Daniel Knauf <daniel.knaut@posteo.de>
2019-10-09 06:46:31 +0200

    update dependencies
    - remove imports

--- app/src/main/java/knaufdan/android/simpletimerapp/di/SimpleTimerApplication.kt
+++ app/src/main/java/knaufdan/android/simpletimerapp/di/SimpleTimerApplication.kt
@@ -1,10 +1,6 @@
 package knaufdan.android.simpletimerapp.di
 
-import android.app.Activity
 import android.app.Application
-import android.app.Service
-import android.content.BroadcastReceiver
-import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
