7c58f6e621c99165d770c15bb94e155e1bea2e93
Daniel Knauf <daniel.knaut@posteo.de>
2020-09-12 19:52:53 +0200

    Kotlin dsl
    - migrate all groovy file to kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        applicationId = "knaufdan.android.simpletimerapp"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    apply(from = "$rootDir/buildSrc/ktlint.gradle.kts")
+
+}
+
+dependencies {
+    "implementation"(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementDependencies()
+}
